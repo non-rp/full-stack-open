f5aea752a67f977192dfd19ebaa2af4adc97f555
Valentyn Melnychenko <non.rp.1996@gmail.com>
2026-02-27 23:27:48 +0200

    completed step 4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -25,30 +25,32 @@ const Content = ({parts}) => {
 
 const Total = ({ exercises }) => {
   console.log(exercises)
-  const total = exercises.reduce((sum, current) => sum + current, 0)
+  const total = exercises.reduce((sum, current) => sum + current.exercises, 0)
   return <p>Number of exercises {total}</p>
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} /> 
-      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
+      <Content parts={parts} /> 
+      <Total exercises={parts} />
     </div>
   )
 }
