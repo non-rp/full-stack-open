15d5299d6910bbee54bf0df880715559d523f0ad
Valentyn Melnychenko <non.rp.1996@gmail.com>
2026-03-01 22:55:59 +0200

    refactor: streamline Statistics component layout and calculations

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,17 +4,13 @@ const Heading = ({ text }) => <h1>{text}</h1>
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const HandleClick = (value, setValue) => {
-  setValue(value + 1)
-}
+const HandleClick = (value, setValue) => setValue(value + 1)
 
-const StatElement = ({ text, value }) => (
-  <p>{text}: {value}</p>
-)
+const StatElement = ({ text, value }) => <tr><td>{text}</td><td>{value}</td></tr>
 
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
-  const average = total === 0 ? 0 :  total / 3
+  const average = total === 0 ? 0 :  (good - bad) / total
   const positive = total === 0 ? 0 : good / total * 100
 
   if (total === 0) {
@@ -22,15 +18,17 @@ const Statistics = ({ good, neutral, bad }) => {
   }
   
   return (
-    <div>
-      <StatElement text="good" value={good} />
-      <StatElement text="neutral" value={neutral} />
-      <StatElement text="bad" value={bad} />
-      <StatElement text="all" value={total} />
+    <table>
+      <tbody>
+        <StatElement text="good" value={good} />
+        <StatElement text="neutral" value={neutral} />
+        <StatElement text="bad" value={bad} />
+        <StatElement text="all" value={total} />
 
-      <StatElement text="average" value={average} />
-      <StatElement text="positive" value={positive + '%'} />
-    </div>
+        <StatElement text="average" value={average.toFixed(1)} />
+        <StatElement text="positive" value={positive.toFixed(1) + '%'} />
+      </tbody>
+    </table >
   )
 }
 
