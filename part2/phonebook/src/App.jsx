8b3373b80b8489429e391eaf55d1ad17cc988ad7
Valentyn Melnychenko <non.rp.1996@gmail.com>
2026-03-30 03:12:26 +0300

    2.11: The Phonebook Step 6 fetch data

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import List from './components/List'
 import Search from './components/Search'
 import Form from './components/Form'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        if(response.status === 200) return response.data
+      })
+      .then( data => { setPersons(data) })
+      .catch(error => console.log(error))
+
+  },[])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
