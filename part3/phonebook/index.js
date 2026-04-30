bcb3d1c98dcc4cf82d33139c6791e75fa3e7c63a
Valentyn Melnychenko <non.rp.1996@gmail.com>
2026-04-30 23:59:57 +0300

    middle ware test

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -82,6 +82,12 @@ app.post('/api/persons', (req, res) => {
   res.json(newPerson)
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}/`)
