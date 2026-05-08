ed9757abbb30b07e1b929f5cd434475138987ac0
Valentyn Melnychenko <non.rp.1996@gmail.com>
2026-05-08 23:18:00 +0300

    Complete exercise 3.10 phonebook backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import morgan from 'morgan'
-import cors from 'cors'
 
 
 morgan.token('user', (req, res) => {
@@ -9,8 +8,8 @@ morgan.token('user', (req, res) => {
 
 const app = express();
 app.use(express.json())
-app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :user'))
+app.use(express.static('dist'))
 
 let persons = [
     { 
@@ -90,7 +89,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}/`)
 })
