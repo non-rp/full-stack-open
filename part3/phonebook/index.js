14defee8adbecca17277aa5c4c7f9e0692e3853a
Valentyn Melnychenko <non.rp.1996@gmail.com>
2026-06-15 23:50:29 +0300

    3.15 and 3.16 implemented

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,19 +28,28 @@ app.get('/info', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
   const id = req.params.id
 
-  Person.findById(id).then(person => {
-    if(person) {
-      res.json(person)
-    } else {
-      return res.status(404).json({ error: `Person id:${id} undefined` })
-    }
-  })
+  Person.findById(id)
+    .then(person => {
+      if(person) {
+        res.json(person)
+      } else {
+        return res.status(404).json({ error: `Person id:${id} undefined` })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      // res.status(500).end()
+      // res.status(400).send({ error: 'malformated id'})
+      next(error) // if without argument jump ti next route or middlewear, if with argument go to error handler middlewear
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  Person.findByIdAndDelete(req.params.id).then(() => {
-    res.status(204).end()
-  })
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(err => next(err))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -55,9 +64,11 @@ app.post('/api/persons', (req, res) => {
     number
   })
 
-  person.save().then(result => {
-    res.json(result)
-  })
+  person.save()
+    .then(result => {
+      res.json(result)
+    })
+    .catch(err => next(err))
 })
 
 app.put('/api/persons/:id', (req, res) => {
@@ -68,16 +79,28 @@ app.put('/api/persons/:id', (req, res) => {
     number
   }
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true }).then(updatedPerson => {
-    res.json(updatedPerson)
-  })
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(err => next(err))
 })
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
+
+errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if(error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
